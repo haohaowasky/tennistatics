9bee344cf9c24a493475a5da33f851e9bcfa3239
Alan Wang <alan.wang@bcgdv.com>
2019-10-20 16:24:09 -0700

    MVP Version

--- app/src/main/java/io/tennis/statistic/Views/PlayActivity.kt
+++ app/src/main/java/io/tennis/statistic/Views/PlayActivity.kt
@@ -6,19 +6,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import androidx.annotation.NonNull
 import com.orhanobut.logger.Logger
 import io.tennis.statistic.R
 import io.tennis.statistic.dataStore.gameData
-import butterknife.BindView
 import butterknife.ButterKnife
-import butterknife.OnClick
-import butterknife.OnTouch
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import io.tennis.statistic.dataStore.AbstractMessage
-import java.security.AccessController.getContext
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 private const val EXTRA_PARAM1 = "io.tennis.statistic.extra.PARAM1"
 private const val EXTRA_PARAM2 = "io.tennis.statistic.extra.PARAM2"
@@ -27,6 +26,9 @@ class PlayActivity : AppCompatActivity() {
 
     private var dataOne= gameData()
     private var dataTwo= gameData()
+    private var serveMap = mutableMapOf<String, gameData>()
+    private lateinit var database: DatabaseReference
+
 
     private lateinit var textView_playerOne: Button
     private lateinit var textView_playerTwo: Button
@@ -35,10 +37,17 @@ class PlayActivity : AppCompatActivity() {
 
     private var serveRole = Serve("", "")
 
+    fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy.MM.dd HH:mm")
+        return format.format(date)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ButterKnife.bind(this)
 //        mPlayerText1 = findViewById<TextView>(R.id.playerOne)
+        database = FirebaseDatabase.getInstance().reference
 
         setContentView(R.layout.activity_play)
         val param1 = intent.getStringExtra(EXTRA_PARAM1)
@@ -46,6 +55,11 @@ class PlayActivity : AppCompatActivity() {
 
         dataOne.playerName = param1
         dataTwo.playerName = param2
+        dataOne.against = param2
+        dataTwo.against = param1
+
+        dataOne.serve = true
+        dataTwo.serve = false
 
         serveRole.serve = param1
         serveRole.respond = param2
@@ -54,9 +68,8 @@ class PlayActivity : AppCompatActivity() {
         textView_playerTwo = findViewById(R.id.Player_2)
 
 
-
-
-
+        serveMap["top"] = dataOne
+        serveMap["down"] = dataTwo
 
         Logger.i("player one name in Player view" +  dataOne.stringify() + "serve term is " + serveRole.stringify())
 
@@ -69,6 +82,20 @@ class PlayActivity : AppCompatActivity() {
         val btnServeOne = findViewById<Button>(R.id.serve_One)
         val btnServeTwo = findViewById<Button>(R.id.serve_Two)
         val btnSaveData = findViewById<Button>(R.id.save_Data)
+
+        val btn_1 = findViewById<Button>(R.id.btn_1)
+        val btn_2 = findViewById<Button>(R.id.btn_2)
+        val btn_3 = findViewById<Button>(R.id.btn_3)
+        val btn_4 = findViewById<Button>(R.id.btn_4)
+        val btn_5 = findViewById<Button>(R.id.btn_5)
+        val btn_6 = findViewById<Button>(R.id.btn_6)
+        val btn_7 = findViewById<Button>(R.id.btn_7)
+        val btn_8 = findViewById<Button>(R.id.btn_8)
+        val btn_9 = findViewById<Button>(R.id.btn_9)
+        val btn_10 = findViewById<Button>(R.id.btn_10)
+        val btn_11 = findViewById<Button>(R.id.btn_11)
+        val btn_12 = findViewById<Button>(R.id.btn_12)
+
 
         btnServeOne.setOnClickListener {
             Logger.i("serve data is " + btnServeOne.text)
@@ -85,6 +112,8 @@ class PlayActivity : AppCompatActivity() {
                         btnServeTwo.setText("Catch")
                     }
             }
+            dataOne.serve = !dataOne.serve
+            dataTwo.serve = !dataTwo.serve
 
         }
 
@@ -103,6 +132,10 @@ class PlayActivity : AppCompatActivity() {
                         btnServeTwo.setText("Catch")
                     }
             }
+
+            dataOne.serve = !dataOne.serve
+            dataTwo.serve = !dataTwo.serve
+
         }
 
         btnSaveData.setOnClickListener{
@@ -110,14 +143,29 @@ class PlayActivity : AppCompatActivity() {
             // TODO add reason in Beta version
             builder.setTitle("Save Data")
             builder.setMessage("Select the winner of the round")
-            builder.setPositiveButton( serveRole.serve,
+            builder.setPositiveButton( dataOne.playerName,
                 DialogInterface.OnClickListener { dialog, id ->
-                    Logger.i("Selected " + serveRole.serve)
+                    dataOne.result = "win"
+                    dataTwo.result = "loose"
+                    Logger.i("upload data " + dataOne.stringify())
+                    Logger.i("upload data " + dataTwo.stringify())
+                    database.child(dataOne.playerName).child((System.currentTimeMillis()).toString()).push().setValue(dataOne)
+                    database.child(dataTwo.playerName).child((System.currentTimeMillis()).toString()).push().setValue(dataTwo)
+
+                    dataOne.spots.clear()
+                    dataTwo.spots.clear()
                     dialog.dismiss()
                 })
-            builder.setNegativeButton(serveRole.respond,
+            builder.setNegativeButton( dataTwo.playerName,
                 DialogInterface.OnClickListener { dialog, id ->
-                    Logger.i("Selected " + serveRole.respond)
+                    dataOne.result = "loose"
+                    dataTwo.result = "win"
+                    Logger.i("upload data " + dataOne.stringify())
+                    Logger.i("Selected " + dataTwo.stringify())
+                    database.child(dataOne.playerName).child((System.currentTimeMillis()).toString()).push().setValue(dataOne)
+                    database.child(dataTwo.playerName).child((System.currentTimeMillis()).toString()).push().setValue(dataTwo)
+                    dataOne.spots.clear()
+                    dataTwo.spots.clear()
                     dialog.dismiss()
                 })
             val alert = builder.create()
@@ -129,9 +177,21 @@ class PlayActivity : AppCompatActivity() {
             // change player2 name to player1
             var buffer = textView_playerTwo.text
             textView_playerTwo.setText(textView_playerOne.text)
-
             //change player1 name to player2
             textView_playerOne.setText(buffer)
+            if (dataOne.playerName == buffer) {
+                serveMap["top"] = dataOne
+                serveMap["down"] = dataTwo
+                Logger.i("changed status")
+            }
+
+            else {
+                serveMap["down"] = dataOne
+                serveMap["top"] = dataTwo
+                Logger.i("changed status")
+
+            }
+
         }
 
 
@@ -140,17 +200,84 @@ class PlayActivity : AppCompatActivity() {
             // change player2 name to player1
             var buffer = textView_playerTwo.text
             textView_playerTwo.setText(textView_playerOne.text)
-
             //change player1 name to player2
             textView_playerOne.setText(buffer)
+
+            if (dataOne.playerName == buffer) {
+                serveMap["top"] = dataOne
+                serveMap["down"] = dataTwo
+                Logger.i("changed status")
+
+            }
+
+            else {
+                serveMap["down"] = dataOne
+                serveMap["top"] = dataTwo
+                Logger.i("changed status")
+
+            }
+
         }
 
+        btn_1.setOnClickListener{
+            serveMap["down"]?.spots!!.add(1)
+            Logger.i("added " + 1)
+        }
 
+        btn_2.setOnClickListener{
+            serveMap["down"]?.spots!!.add(2)
+            Logger.i("added " + 2)
+        }
 
+        btn_3.setOnClickListener{
+            serveMap["down"]?.spots!!.add(3)
+            Logger.i("added " + 3)
+        }
 
+        btn_4.setOnClickListener{
+            serveMap["down"]?.spots!!.add(4)
+            Logger.i("added " + 4)
+        }
 
+        btn_5.setOnClickListener{
+            serveMap["down"]?.spots!!.add(5)
+            Logger.i("added " + 5)
+        }
 
+        btn_6.setOnClickListener{
+            serveMap["down"]?.spots!!.add(6)
+            Logger.i("added " + 6)
+        }
 
+        btn_7.setOnClickListener{
+            serveMap["top"]?.spots!!.add(7)
+            Logger.i("added " + 7)
+        }
+
+        btn_8.setOnClickListener{
+            serveMap["top"]?.spots!!.add(8)
+            Logger.i("added " + 8)
+        }
+
+        btn_9.setOnClickListener{
+            serveMap["top"]?.spots!!.add(9)
+            Logger.i("added " + 9)
+        }
+
+        btn_10.setOnClickListener{
+            serveMap["top"]?.spots!!.add(10)
+            Logger.i("added " + 10)
+        }
+
+        btn_11.setOnClickListener{
+            serveMap["top"]?.spots!!.add(11)
+            Logger.i("added " + 11)
+        }
+
+        btn_12.setOnClickListener{
+            serveMap["top"]?.spots!!.add(12)
+            Logger.i("added " + 12)
+        }
     }
 
     override fun onStart() {
--- app/src/main/java/io/tennis/statistic/dataStore/gameData.kt
+++ app/src/main/java/io/tennis/statistic/dataStore/gameData.kt
@@ -3,18 +3,18 @@ import java.util.ArrayList
 
 
 class gameData :  AbstractMessage() {
-    var spots: MutableList<MutableList<Int>> = ArrayList()
+    var spots: MutableList<Int> = ArrayList()
     var serve: Boolean = false
     var playerName: String = ""
     var result:String =  ""
-    var notes:String = ""
+    var against: String = ""
 
-    fun gameData(playerName: String, serve: Boolean,  spots: MutableList<MutableList<Int>>, result:String, notes: String) {
+    fun gameData(playerName: String, serve: Boolean,  spots: MutableList<Int>, result:String, against: String) {
         this.playerName = playerName
         this.serve = serve
         this.spots = spots
         this.result = result
-        this.notes = notes
+        this.against = against
     }
 }
 
